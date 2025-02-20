fixing variance loss + more setting in the config

inference.py
@@ -109,15 +109,27 @@ def main():
         "--checkpoint", type=str, default=None,
         help="Path to the model checkpoint. If not provided, will use default from config."
     )
-    # Only required for embedding-test
     parser.add_argument(
         "--compare-images", type=str, nargs="+",
         help="List of image paths to compare with (required for embedding-test)."
     )
+    parser.add_argument(
+        "--config-path", type=str, default="", 
+        help="Path to the config file. If empty or invalid, uses 'config.yaml' in the current directory."
+    )
+
     args = parser.parse_args()
 
-    cfg = load_config()
+    # Determine config file path
+    config_path = args.config_path.strip()
+    if not config_path or not os.path.exists(config_path):
+        config_path = os.path.join(os.path.dirname(__file__), "config.yaml")
+        print(f"Using default config: {config_path}")
+
+    # Load configuration
+    cfg = load_config(config_path)
     print(cfg)
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Initialize models

train.py
@@ -12,7 +12,13 @@ from utils import *
 import torch.nn as nn
 from tqdm import tqdm
 
-def train_step(face_model, generator, batch, optimizer_face, optimizer_gen, device):
+def train_step(face_model, 
+               generator, 
+               batch, 
+               optimizer_face, 
+               optimizer_gen, 
+               device, 
+               cfg):
     """
     Performs one training step.
     
@@ -57,7 +63,8 @@ def train_step(face_model, generator, batch, optimizer_face, optimizer_gen, devi
     mse_loss_face = nn.MSELoss()(generated_images_for_face, images)
     
     # Total embedding loss: you might consider weighting the variance loss if needed.
-    embedding_loss = variance_loss + mse_loss_face  # You could do: lambda1*variance_loss + lambda2*mse_loss_face
+    embedding_loss = (variance_loss * cfg['variance_loss_impact'] 
+                      + mse_loss_face * cfg['gen_loss_impact'])  # You could do: lambda1*variance_loss + lambda2*mse_loss_face
     embedding_loss.backward()
     optimizer_face.step()
     
@@ -270,7 +277,7 @@ def train():
                              else torch.stack([b[key] for b in batch]).to(device)
                              for key in batch[0].keys()}
 
-                    embedding_loss, gen_loss = train_step(face_model, generator, batch, optimizer_face, optimizer_gen, device)
+                    embedding_loss, gen_loss = train_step(face_model, generator, batch, optimizer_face, optimizer_gen, device, cfg)
 
                     total_embedding_loss += embedding_loss
                     total_gen_loss += gen_loss